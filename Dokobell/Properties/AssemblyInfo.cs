2f0d95ac3d57140e2f842d4210038dec0f480257
kaisei kawashima <kaisei.kawashima@gmail.com>
2024-03-01 14:08:56 +0900

    add fordy library

--- Dokobell/Properties/AssemblyInfo.cs
+++ Dokobell/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dokobell")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©k-kawaa  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
